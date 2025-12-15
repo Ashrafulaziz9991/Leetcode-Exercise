569d22cc6ed3ca47b186ea240f0139d5f38e9340
Ashrafulaziz9991 <ashrafulaziz9991@gmail.com>
2025-12-15 23:47:57 +0600

    done

---
+++ Sort Colors.cpp
@@ -0,0 +1,8 @@
+//problem link : https://leetcode.com/problems/sort-colors/?envType=problem-list-v2&envId=sorting
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+    }
+};
