ae97b5134fe756b4d3458384e54951a619e4323e
Ashrafulaziz9991 <ashrafulaziz9991@gmail.com>
2025-12-24 16:10:22 +0600

    done

---
+++ problem50.cpp
@@ -0,0 +1,9 @@
+// problem link : https://leetcode.com/problems/powx-n/description/?envType=problem-list-v2&envId=math
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double result = pow(x, n);
+        return result;
+    }
+};
