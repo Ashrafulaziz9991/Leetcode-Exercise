5484c257aed54e5b1579b0ef3469c731102cde77
Ashrafulaziz9991 <ashrafulaziz9991@gmail.com>
2026-01-03 20:57:22 +0600

    changed

--- Remove Duplicates from Sorted Array.cpp
+++ Remove Duplicates from Sorted Array.cpp
@@ -14,7 +14,7 @@ int main()
         return 0;
     }
     int counter = 1;
-    for (int i = 0; i < nums.size(); i++)
+    for (int i = 1; i < nums.size(); i++)
     {
         if (nums[i] != nums[i - 1])
         {
