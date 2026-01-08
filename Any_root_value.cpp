06abd217702e1865ecf1d8a0e4ea46273223a708
Ashrafulaziz9991 <ashrafulaziz9991@gmail.com>
2026-01-08 21:23:04 +0600

    New added any root value of a Number

---
+++ Any_root_value.cpp
@@ -0,0 +1,30 @@
+/**
+ * Newton–Raphson (Universal Root finding Method)
+ * it works for any n-th root value of a Number
+ */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double nthRoot(double A, int n)
+{
+    double x = A;      // initial guess
+    double eps = 1e-6; // precision
+
+    while (true)
+    {
+        double x_next = ((n - 1) * x + A / pow(x, n - 1)) / n;
+        if (abs(x - x_next) < eps)
+            break;
+        x = x_next;
+    }
+    return x;
+}
+
+int main()
+{
+    cout << "Square root of 25: " << nthRoot(25, 2) << endl;
+    cout << "Cube root of 81: " << nthRoot(81, 2) << endl;
+    cout << "4th root of 16: " << nthRoot(16, 2) << endl;
+}
