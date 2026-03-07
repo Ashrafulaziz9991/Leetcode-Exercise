2b591fcc9c7903155dd11f6fcc14eddbd1acb4ff
Ashrafulaziz9991 <ashrafulaziz9991@gmail.com>
2026-03-07 16:55:57 +0600

    little bit removed

--- Contains Duplicate II.cpp
+++ Contains Duplicate II.cpp
@@ -12,39 +12,24 @@ int main()
     bool flag = false;
     int k = 3;
 
-    // for (int i = 0; i < nums.size(); i++)
-    // {
-    //     for (int j = i + 1; j < nums.size(); j++)
-    //     {
-    //         if(nums[i] == nums[j]){
-    //             if(abs(i - j) <= k){
-    //                 flag = true;
-    //                 break;
-    //             }
+    // O(n^2)
+    for (int i = 0; i < nums.size(); i++)
+    {
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            if(nums[i] == nums[j]){
+                if(abs(i - j) <= k){
+                    flag = true;
+                    break;
+                }
                 
-    //         }
+            }
             
-    //     }
-    // }
-    // if(flag) cout << "YES";
-    // else cout << "NO";
+        }
+    }
+    if(flag) cout << "YES";
+    else cout << "NO";
 
-    // map<int, int> freq;
-    // for(int i : nums) freq[i]++;
-    // for (int i = 0; i < nums.size(); i++)
-    // {
-    //     vector<int>temp;
-    //     if(freq[nums[i]] > 1){
-    //         temp.push_back(i);
-    //         for (int j = 0; j < temp.size() - 1; j++)
-    //         {
-    //             if(abs(temp[j] - temp[j + 1]) <= k){
-    //                 flag = true;
-    //                 break;
-    //             }
-    //         }
-    //     }
-    // }
 
     unordered_map<int, int> Seenbefore;
     for (int i = 0; i < nums.size(); i++)
