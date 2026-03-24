d9f70eed11cf0f399526fac07475d6cc276b35af
Ashrafulaziz9991 <ashrafulaziz9991@gmail.com>
2026-03-24 23:47:26 +0600

    Donw now

---
+++ Transform Array by Parity.cpp
@@ -0,0 +1,16 @@
+// problem link : https://leetcode.com/problems/transform-array-by-parity/
+
+
+class Solution {
+public:
+    vector<int> transformArray(vector<int>& nums) {
+        for(int &i : nums){
+            if(i % 2 == 0)
+                i = 0;
+            else
+                i = 1;
+        }
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};
