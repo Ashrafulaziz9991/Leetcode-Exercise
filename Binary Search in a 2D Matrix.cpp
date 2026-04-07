301e6d65106760b8d842e8401bfaa61ff6877af4
Ashrafulaziz9991 <ashrafulaziz9991@gmail.com>
2026-04-07 23:22:17 +0600

    Done & added solution 2

--- Binary Search in a 2D Matrix.cpp
+++ Binary Search in a 2D Matrix.cpp
@@ -1,5 +1,6 @@
 // problem link : https://leetcode.com/problems/search-a-2d-matrix/description/
 
+// solution 1 :
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
@@ -17,6 +18,26 @@ public:
                 low = mid + 1;
             else
                 high = mid - 1;
+        }
+        return false;
+    }
+};
+
+
+// solution 2:
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+        int row = 0;
+        int col = n - 1;
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target)
+                return true;
+            else if (matrix[row][col] < target)
+                row = row + 1;
+            else
+                col = col - 1;
         }
         return false;
     }
