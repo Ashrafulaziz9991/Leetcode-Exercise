9616ef57b8ea0cf18699421105833c2dd153f43b
Ashrafulaziz9991 <ashrafulaziz9991@gmail.com>
2026-04-08 19:38:35 +0600

    Done now

---
+++ Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,14 @@
+// problem link : 
+// the problem has requerd O(log n), but we did it in O(n), we'll make that later
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int mn = nums[0];
+
+        for(int i : nums)
+            if(i < mn)
+                mn = i;
+        return mn;
+    }
+};
