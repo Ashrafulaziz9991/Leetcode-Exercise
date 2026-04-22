59305c08b30f8a0bdf477e081a9e9e31a6b384b2
Ashrafulaziz9991 <ashrafulaziz9991@gmail.com>
2026-04-22 20:08:50 +0600

    Not finished

---
+++ Remove Digit From Number to Maximize Result.cpp
@@ -0,0 +1,12 @@
+// problem link : https://leetcode.com/problems/remove-digit-from-number-to-maximize-result/description/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    string number = "123";
+    char digit = '3';
+    return 0;
+}
