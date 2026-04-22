cea460e98ab01a08d0221255cf7342994ef4d649
Ashrafulaziz9991 <ashrafulaziz9991@gmail.com>
2026-04-22 19:51:09 +0600

    not finished

---
+++ Candy.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    vector<int>ratings= {};
+
+    int mx_cnt = 0, mx = ratings[0];
+
+    for (int i = 0; i < ratings.size(); i++)
+    {
+        
+    }
+    
+
+    return 0;
+}
